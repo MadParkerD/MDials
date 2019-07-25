cb8948bab5e733f0c63c1083bacacca34e540d94
Madison Parker-Durost <madison.parkerdurost@gmail.com>
2019-07-25 16:25:00 -0700

    Fixed multiplexing, and some misunderstandings about object intialization

--- mdials-source/mdials-source.ino
+++ mdials-source/mdials-source.ino
@@ -5,19 +5,16 @@
 Potentiometer c1;
 Potentiometer c2;
 Potentiometer c3;
-const int numPots=1;
+
+//Change numPots to your number of potentiometers
+const int numPots=2;
 Potentiometer pots[numPots];
 
 void setup() {
 /*Define pin type here. Your arduino may have a different
   pin configuration but as a general practice pins should
   be set to output.*/
-  pinMode(6,OUTPUT);
-  pinMode(7,OUTPUT);
-  pinMode(8,OUTPUT);
-  pinMode(9,OUTPUT);
-  pinMode(10,OUTPUT);
-  pinMode(11,OUTPUT);
+  
 
 /*Set pins here
   Pins should be set with one pin per voltage/ground on your 
@@ -30,7 +27,8 @@ void setup() {
   c1.pin2 = 6;
   
   Reader should correspond to the analog pin connected to
-  the signal pin of the potentiometer
+  the signal pin of the potentiometer. 
+  Typically A0-A<number of analog pins -1>
   c1.reader = A2;
   
   Value is the actual value read by the potentiometer, 
@@ -39,55 +37,60 @@ void setup() {
   
   Channel is the midi channel to attempt to use. 
   1-16 in practice, but some midi devices may support more
-  Logically this is not as important if being use by modern software, 
-  as many things have an "all channels" option
+  Logically this is not as important if being use by modern 
+  software, as many things have an "all channels" option
   c1.channel = 0;
   
-  Control is the cc number to send. Different midi devices treat these
-  cc numbers differently. As per the midicc standard
+  Control is the cc number to send. Different midi devices treat 
+  these cc numbers differently. As per the midicc standard
   at least 1-63 support values of 0-127 which the potentiometer
   class will attempt to send, so use those unless you have 
-  more potentiometers. As this program is designed primarily for vcvrack 
-  which allows any cc number to be read, any cc number that supports the full
-  1-127 should work. Values above 31(LSB values) SHOULD be handled by the 
-  potentiometer class, but some manufacturers treat these as special and may 
-  support values of larger sizes (128x128 with 7 bit msb and lsb aka 14 bit midicc`) which this 
-  program does NOT support without modification. Devices that treat these values
-  as bank selects etc. are also not supported (This mean NO hardware midi devices
+  more potentiometers. As this program is designed primarily for 
+  vcvrack which allows any cc number to be read, any cc number 
+  that supports the full 1-127 should work. 
+  Values above 31(LSB values) SHOULD be handled by the 
+  potentiometer class, but some manufacturers treat these as 
+  special and may support values of larger sizes which this 
+  program does NOT support without modification.
+  (e.g. 128x128 with 7 bit msb and lsb aka 14 bit midicc)  
+  Devices that treat these values as bank selects etc. are 
+  also not supported (This mean almost no hardware midi devices
   are supported).
   c1.contol = 0;*/
 
-
-  c1.pin1 = 6;
-  c1.pin2 = 7;
-  c1.reader = A0;
+  c1.pin1 = 4;
+  c1.pin2 = 5;
+  c1.reader = 14;
   c1.value = 0;
   c1.channel = 0;
   c1.control = 0;
+
   
-  c2.pin1 = 9;
-  c2.pin2 = 8;
-  c2.reader = A1;
+  c2.pin1 = 6;
+  c2.pin2 = 7;
+  c2.reader = 14;
   c2.value = 0;
   c2.channel = 0;
   c2.control = 0;
 
-  c3.pin1 = 10;
+  c3.pin1 = 3;
   c3.pin2 = 11;
-  c3.reader = A0;
+  c3.reader = 14;
   c3.value = 0;
   c3.channel = 0;
   c3.control = 0;
-  
-//Change numPots to your number of potentiometers
-  Potentiometer pots[numPots] = {c1};
+
+//Add all potentiometers declared above here
+  Potentiometer init[numPots] = {c1,c2};
+  for (int i = 0; i < numPots; i++)
+  {
+    pots[i] = init[i];
+  }
   Serial.begin(9600);
 }
 
 // the loop routine runs over and over again forever:
 void loop() {
-  /*Read the input on analog pin 0, setting one pin to high 
-    and one to ground*/
   for (int i = 0; i < numPots; i++)
   {
     pots[i].read();
@@ -98,13 +101,11 @@ void loop() {
     }
   }
   Serial.println("");
-  //int sv2 = c2.read();
-  //int sv3 = c3.read();
+
   USBMIDI.poll();
   while (USBMIDI.available()) {
     u8 message = USBMIDI.read();
   }
-
   // Flush the output.
   USBMIDI.flush();
   
--- mdials-source/potentiometer.cpp
+++ mdials-source/potentiometer.cpp
@@ -4,63 +4,112 @@ class Potentiometer
 {
   public:
 
-  int pin1;
+    int pin1;
 
-  int pin2;
+    int pin2;
 
-  int value;
+    int value;
   
-  uint8_t channel;
+    uint8_t channel;
 
-  uint8_t control;
+    uint8_t control;
 
-  uint8_t reader;
-
-  void up()
-  {
+    uint8_t reader;
+    
+   /*
+   * This function brings the potentiometer into a readable state
+   */
+    void up()
+    {
+    pinMode(pin1,OUTPUT);
+    pinMode(pin2,OUTPUT);
+    delay(10);
     digitalWrite(pin1,HIGH);
     digitalWrite(pin2,LOW);
-    delay(2);
-  }
-  
-  void down()
-  {
-    digitalWrite(pin1,LOW);
-    digitalWrite(pin2,LOW);
-    delay(2);
-  }
-
-  int read()
-  {
-    up();
-    value = analogRead(reader);
-    down();
-    delay(5);
-    return value;
-  }
-
-  bool changed(int newValue)
-  {
-    if (newValue/8 != value/8)
-    {
-      return true;
+    delay(10);
     }
-    return false;
-  }
 
-  bool send()
-  {
-    if(changed(read()))
+  /* 
+   *  This function brings the potentiometer out of readable state
+   */
+    void down()
     {
+      digitalWrite(pin1,LOW);
+      digitalWrite(pin2,LOW);
+      delay(10);
+      pinMode(pin1,INPUT);
+      pinMode(pin2,INPUT);
+      delay(10);
+
+    }
+
+
+  /*
+   * This function reads the potentiometer
+   * Uses the "char" passed in when creating the potentiometer
+   * for reading, this is generally A0-Ax depending on the
+   * number of analog pins on your arduino.
+   */
+    int read()
+    {
+      up();
+      delay(10);
+      analogRead(reader);
+      value = analogRead(reader);
+      delay(10);
+      down();
+      delay(5);
+      return value;
+    }
+
+  /*
+   *To avoid saturating the interface this function checks a 
+   *value to see if it is changed. No real point in sending values 
+   *if they are not changed because this is using midiCC 
+   *and a send does not correspond to a played note but rather a 
+   *change in a continous value.
+   */
+    bool changed(int newValue)
+    {
+      if (newValue/8 != value/8)
+      {
+        return true;
+      }
+      return false;
+    }
+
+  /*
+   * Send midi control change
+   * Value/8 here is implicitly cast to uint8_t
+   * This is fine because the maximum read for
+   * analog pins is 1023 and 1023/8 < 255
+   * The write calls themselves are substantially sumilar to 
+   * the midictrl example provided by usbmidi on their github:
+   * https://github.com/BlokasLabs/USBMIDI/blob/master/examples/midictrl/midictrl.ino
+   */
+    bool send()
+    {
+      if(changed(read()))
+      {
+        uint8_t val = value/8;      
         USBMIDI.write(0xB0 | (channel & 0xf));
         USBMIDI.write(control & 0x7f);
-        USBMIDI.write(value & 0x7f);
-    }
+        USBMIDI.write(val & 0x7f);
+      }
     
-  }
+    }
 
-  void print()
-  {
-    Serial.print(value/8);
-  }
+  /*
+   * Print the value that would be sent via midiCC
+   * Mostly useful for debug
+   */
+    void print()
+    {
+      
+      Serial.print(pin1);
+      Serial.print(" | ");
+      Serial.print(pin2);
+      Serial.print(" | ");
+      Serial.print(value);
+    }
 };
